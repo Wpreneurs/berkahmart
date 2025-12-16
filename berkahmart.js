59809429799cea3b9cf2cad4b26454425442f683
Wpreneurs <114202814+Wpreneurs@users.noreply.github.com>
2025-12-16 19:01:15 +0700

    Refactor service worker caching logic

--- berkahmart.js
+++ berkahmart.js
@@ -6,7 +6,7 @@ self.addEventListener("install", e => {
         "/?m=1",  // versi mobile
         "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
         "https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap",
-        "https://blogger.googleusercontent.com/img/.../LOGO-SIMPELTOKO-Square-300x300.jpg"
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi8k_pPGR8SLcvTEWba2jN-E5k2NCYSNMrHLwMbRxe2rupBDSEUEOgKYb4HdlTUnhbAnEDI6jRa_RTV6F1-n8jTHH7dF8v-zkqZ9BzggXVBkZ2yhggIGmrOblG5uZa3wreds-pGwLwZ5UhLAq5mbBw3zqsCW6aHQszkqiuwydFZ9HOiqvy2kEDJudXwZXVq/s1600/512x512.png"
       ]);
     })
   );
@@ -19,3 +19,4 @@ self.addEventListener("fetch", e => {
     })
   );
 });
+
